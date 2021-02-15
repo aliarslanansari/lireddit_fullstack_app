33112a9ef8b7b602bc243fe9d8461d78338ff402
Ali Arslan <aliarslan1620@gmail.com>
2021-02-16 02:47:09 +0530

    User resolver for registeration done

---
+++ src/resolvers/user.ts
@@ -0,0 +1,26 @@
+import { Post } from "../entities/Post"
+import { MyContext } from "src/types"
+import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql"
+import { User } from "src/entities/User"
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string
+
+  @Field()
+  password: string
+}
+
+@Resolver()
+export class UserResolver {
+  @Mutation(() => Post)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ) {
+    const user = em.create(User, { username: options.username })
+    await em.persistAndFlush(user)
+    return "bye"
+  }
+}
