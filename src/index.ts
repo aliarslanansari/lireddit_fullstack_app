1519f0abc67d02f8147636d5f99ed26993e80535
Ali Arslan <aliarslan1620@gmail.com>
2021-02-16 01:34:59 +0530

    initial graphQL and Express Setup done

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
 import { MikroORM } from "@mikro-orm/core"
 import { __prod__ } from "./constants"
-import { Post } from "./entities/Post"
+// import { Post } from "./entities/Post"
+import microConfig from "./mikro-orm.config"
+import express from "express"
+import { ApolloServer } from "apollo-server-express"
+import { buildSchema } from "type-graphql"
+import { HelloResolver } from "./resolvers/hello"
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: "lireddit",
-    type: "postgresql",
-    debug: !__prod__,
+  const orm = await MikroORM.init(microConfig)
+  await orm.getMigrator().up()
+
+  const app = express()
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
   })
-  const post = orm.em.create(Post, { title: "my first post" })
-  await orm.em.persistAndFlush(post)
-  
+
+  apolloServer.applyMiddleware({ app })
+
+  app.listen(4000, () => {
+    console.log("Server started on localhost:4000")
+  })
 }
 
-main()
-console.log("Hell1o")
+main().catch((err) => {
+  console.log(err)
+})
