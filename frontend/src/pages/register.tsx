f93b33b0b40a9ac9342fdd72c2c4967f202f5c5e
Ali Arslan <aliarslan1620@gmail.com>
2021-02-17 02:33:14 +0530

    NextJS Register Page Validation Done

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -3,16 +3,26 @@ import { Form, Formik } from "formik"
 import Wrapper from "../components/Wrapper"
 import InputField from "../components/InputField"
 import { Box, Button } from "@chakra-ui/react"
+import { useRegisterMutation } from "../generated/graphql"
+import { toErrorMap } from "../utils/toErrorMap"
+import { useRouter } from "next/router"
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter()
+  const [, register] = useRegisterMutation()
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          console.log({ values })
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values)
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors))
+          } else if (response.data?.register.user) {
+            router.push('/')
+          }
         }}>
         {({ isSubmitting }) => (
           <Form>
@@ -29,7 +39,11 @@ const Register: React.FC<registerProps> = ({}) => {
                 type="password"
               />
             </Box>
-            <Button mt={4} type="submit" colorScheme="teal" isLoading={isSubmitting}>
+            <Button
+              mt={4}
+              type="submit"
+              colorScheme="teal"
+              isLoading={isSubmitting}>
               Register
             </Button>
           </Form>
