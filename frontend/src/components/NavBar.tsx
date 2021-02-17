ec347415ef6ad9ffb58e98925e3c3eb973e80594
Ali Arslan <aliarslan1620@gmail.com>
2021-02-17 23:18:16 +0530

    Added NavBar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,20 +1,36 @@
-import { Box, Flex, Link } from "@chakra-ui/react"
+import { Box, Button, Flex, Link } from "@chakra-ui/react"
 import React from "react"
 import NextLink from "next/link"
+import { useMeQuery } from "../generated/graphql"
 
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = ({}) => {
-  return (
-    <Flex bg="tomato" p={4}>
-      <Box ml="auto">
+  const [{ data, fetching }] = useMeQuery()
+  let body = null
+  if (fetching) {
+  } else if (!data?.me) {
+    body = (
+      <>
         <NextLink href="/login">
           <Link mr={2}>login</Link>
         </NextLink>
         <NextLink href="/register">
           <Link>register</Link>
         </NextLink>
-      </Box>
+      </>
+    )
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data?.me?.username}</Box>
+        <Button variant='link'>logout</Button>
+      </Flex>
+    )
+  }
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml="auto">{body}</Box>
     </Flex>
   )
 }
