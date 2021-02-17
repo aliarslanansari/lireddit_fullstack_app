68a8f5929d2e81e73b833fdf4113c6ca19939d1e
Ali Arslan <aliarslan1620@gmail.com>
2021-02-18 01:27:09 +0530

    refactored code

--- backend/src/resolvers/user.ts
+++ backend/src/resolvers/user.ts
@@ -3,7 +3,6 @@ import {
   Arg,
   Ctx,
   Field,
-  InputType,
   Mutation,
   ObjectType,
   Query,
@@ -12,15 +11,8 @@ import {
 import argon2 from "argon2"
 import { User } from "../entities/User"
 import { COOKIE_NAME } from "../constants"
-
-@InputType()
-class UsernamePasswordInput {
-  @Field()
-  username: string
-
-  @Field()
-  password: string
-}
+import { UsernamePasswordInput } from "./UsernamePasswordInput"
+import { validateRegister } from "src/utils/validateRegister"
 
 @ObjectType()
 class FieldError {
@@ -61,28 +53,13 @@ export class UserResolver {
     @Arg("options") options: UsernamePasswordInput,
     @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
-    if (options.username.length <= 2) {
-      return {
-        errors: [
-          {
-            field: "username",
-            message: "Username must be greater atleast 3 character",
-          },
-        ],
-      }
-    }
-    if (options.password.length <= 3) {
-      return {
-        errors: [
-          {
-            field: "password",
-            message: "password must be atleast 4 character",
-          },
-        ],
-      }
+    const errors = validateRegister(options)
+    if (errors) {
+      return { errors }
     }
     const hashedPassword = await argon2.hash(options.password)
     const user = em.create(User, {
+      email: options.email,
       username: options.username,
       password: hashedPassword,
     })
@@ -110,10 +87,16 @@ export class UserResolver {
 
   @Mutation(() => UserResponse)
   async login(
-    @Arg("options") options: UsernamePasswordInput,
+    @Arg("usernameOrEmail") usernameOrEmail: string,
+    @Arg("password") password: string,
     @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
-    const user = await em.findOne(User, { username: options.username })
+    const user = await em.findOne(
+      User,
+      usernameOrEmail.includes("@")
+        ? { email: usernameOrEmail }
+        : { username: usernameOrEmail }
+    )
     if (!user) {
       return {
         errors: [
@@ -124,7 +107,7 @@ export class UserResolver {
         ],
       }
     }
-    const valid = await argon2.verify(user.password, options.password)
+    const valid = await argon2.verify(user.password, password)
     if (!valid) {
       return {
         errors: [
---
+++ backend/src/utils/validateRegister.ts
@@ -0,0 +1,36 @@
+import { UsernamePasswordInput } from "../resolvers/UsernamePasswordInput"
+
+export const validateRegister = (options: UsernamePasswordInput) => {
+  if (!options.email.includes("@") || !options.email.includes(".")) {
+    return [
+      {
+        field: "email",
+        message: "Please enter valid Email Id",
+      },
+    ]
+  }
+  if (options.username.length <= 2) {
+    return [
+      {
+        field: "username",
+        message: "Username must be greater atleast 3 character",
+      },
+    ]
+  }
+  if (options.username.includes('@')) {
+    return [
+      {
+        field: "username",
+        message: "Username cannot contain @",
+      },
+    ]
+  }
+  if (options.password.length <= 3) {
+    return [
+      {
+        field: "password",
+        message: "password must be atleast 4 character",
+      },
+    ]
+  }
+}
