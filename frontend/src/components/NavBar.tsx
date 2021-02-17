ddf81776e6eddc871fb47d6ac0bddbd0c1797805
Ali Arslan <aliarslan1620@gmail.com>
2021-02-17 23:40:50 +0530

    caching user on login register mutation

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -29,7 +29,7 @@ const NavBar: React.FC<NavBarProps> = ({}) => {
     )
   }
   return (
-    <Flex bg="tomato" p={4}>
+    <Flex bg="tan" p={4}>
       <Box ml="auto">{body}</Box>
     </Flex>
   )
